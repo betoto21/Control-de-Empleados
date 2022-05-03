4ce7ad8d20d9079fb5f9a88e6d00ccadbf810b63
Daniel Alberto Felix Bustamante <danielcoo.dafb@gmail.com>
2022-05-02 18:58:09 -0700

    Muestra en tabla a los empleados y genera la entra y salida de los empleados

--- src/conexion/Conexion.java
+++ src/conexion/Conexion.java
@@ -1,11 +1,14 @@
 package conexion;
 
+import com.google.gson.Gson;
 import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
 import com.mongodb.DBCursor;
 import com.mongodb.Mongo;
 import java.net.UnknownHostException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import javax.swing.JOptionPane;
 
 /**
@@ -14,9 +17,9 @@ import javax.swing.JOptionPane;
  */
 public class Conexion {
 
-    DB db;
-    DBCollection coleccionEmpleados;
-    DBCollection coleccionChecks;
+    private DB db;
+    private DBCollection coleccionEmpleados;
+    private DBCollection coleccionChecks;
 
     public Conexion() {
         try {
@@ -29,54 +32,106 @@ public class Conexion {
             x.showMessageDialog(null, "Hubo un error de conexion; \n" + ex.getMessage());
         }
     }
-    public int genId(){
-        int id = (int) (Math.random()*100000);
+
+    public int genId() {
+        int id = (int) (Math.random() * 100000);
         return id;
     }
-    public String[] getEmpleados(){
+
+    public String[] getEmpleados() {
         DBCursor cursor = coleccionEmpleados.find();
         String datos = "";
-        while(cursor.hasNext()){
+        while (cursor.hasNext()) {
             datos = datos + cursor.next().toString() + "\n";
         }
         String[] arr = datos.split("\n");
         return arr;
     }
-    public String[] getCheck(){
-        DBCursor cursor = coleccionChecks.find();
-        String datos = "";
-        while(cursor.hasNext()){
-            datos = datos + cursor.next().toString() + "\n";
-        }
-        String[] arr = datos.split("\n");
-        return arr;
-    }
-    public void addEmpleados(String nombre, String puesto,String foto){
+
+    public void addEmpleados(String nombre, String puesto, String foto) {
         BasicDBObject document = new BasicDBObject();
-        document.put("Id", genId());
+        int id = 0;
+        String[] arr = getEmpleados();
+        for (String x : arr) {
+            Gson g = new Gson();
+            Empleados em = g.fromJson(x, Empleados.class);
+            int idno = em.Id;
+            do {
+                id = genId();
+            } while (id == em.Id);
+        }
+        document.put("Id", id);
         document.put("Nombre", nombre);
         document.put("Puesto", puesto);
         document.put("Foto", foto);
         coleccionEmpleados.insert(document);
     }
-    public void updateEmpleados(int id, String nombre, String puesto, String foto){
+
+    public void updateEmpleados(int id, String nombre, String puesto, String foto) {
         BasicDBObject newDatos = new BasicDBObject();
         BasicDBObject filtro = new BasicDBObject();
         BasicDBObject update = new BasicDBObject();
-        
-        newDatos.append("Id", id);
+
         newDatos.append("Nombre", nombre);
         newDatos.append("Puesto", puesto);
         newDatos.append("Foto", foto);
         update.append("$set", newDatos);
-        
+
         filtro.append("Id", id);
         coleccionEmpleados.updateMulti(filtro, update);
     }
-    public void deleteEmpleados(int id){
+
+    public void deleteEmpleados(int id) {
         BasicDBObject document = new BasicDBObject();
         document.append("Id", id);
         coleccionEmpleados.remove(document);
     }
+
+    public String[] getCheck() {
+        DBCursor cursor = coleccionChecks.find();
+        String datos = "";
+        while (cursor.hasNext()) {
+            datos = datos + cursor.next().toString() + "\n";
+        }
+        String[] arr = datos.split("\n");
+        return arr;
+    }
+
+    public void addCheckIn(int idEmpleado) {
+        BasicDBObject document = new BasicDBObject();
+        int id = 0;
+        String[] arr = getCheck();
+        for(String x : arr){
+            Gson g = new Gson();
+            Checks ch = g.fromJson(x, Checks.class);
+            int idno = ch.IdCheck;
+            do{
+                id = genId();
+            }while(id == ch.IdCheck);
+        }
+        document.put("IdCheck", id);
+        document.put("IdEmpleado", idEmpleado);
+        document.put("Entrada", getFecha());
+        document.put("Salida", " ");
+        coleccionChecks.insert(document);
+
+    }
     
+    public void addCheckOut(int idCheck){
+        BasicDBObject newDatos = new BasicDBObject();
+        BasicDBObject filtro = new BasicDBObject();
+        BasicDBObject update = new BasicDBObject();
+        
+        newDatos.append("Salida", getFecha());
+        update.append("$set", newDatos);
+        
+        filtro.append("IdCheck", idCheck);
+        coleccionChecks.updateMulti(filtro,update);
+    }
+    
+    public String getFecha(){
+        DateTimeFormatter formato = DateTimeFormatter.ofPattern("yy/MM/dd HH:mm:ss");
+        String fecha = formato.format(LocalDateTime.now());
+        return fecha;
+    }
 }
